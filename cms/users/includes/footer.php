996719d1858521f4c91a7a96df9443b150b9c2ee
ManojAthreya <manoj2375@gmail.com>
2019-11-28 18:20:54 +0530

    New changes made

---
+++ cms/users/includes/footer.php
@@ -0,0 +1,7 @@
+ <footer class="site-footer">
+          <div class="text-center">
+              2019 - MCC              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
